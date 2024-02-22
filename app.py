a5fdb590c3cf65e52db509da6a989a5c7a76605c
febnn <akpanov@yandex.ru>
2024-02-22 13:38:00 +0100

    control point

--- app.py
+++ app.py
@@ -14,31 +14,34 @@ Session(app)
 
 db = SQL("sqlite:///tasks.db")
 
-icons = [
-    '/static/clock-svgrepo-com.svg',
-    '/static/knowledge-svgrepo-com.svg',
-    '/static/e-learning-svgrepo-com.svg',
-    '/static/research-svgrepo-com.svg',
-    '/static/student-svgrepo-com.svg',
-    '/static/schedule-svgrepo-com.svg'
-]
+links = [{"in_progress":'/in_progress'}, {'completed': '/completed'}, {'wont_do': '/wont_do'}]
 
 
 # MAIN PAGE
 @app.route('/')
 @login_required
 def index():
-    return render_template('main.html')
+    return render_template('main.html', link=links[0])
 
 # CREATE PAGE
 @app.route('/create', methods=['GET', 'POST'])
 @login_required
 def create():
+    user_id = session['user_id']
+    error_title = None
+    error_status = None
     if request.method == 'POST':
-        status = request.form.get('status')
+        title = request.form.get('title')
+        description = request.form.get('description')
         icon = request.form.get('icon')
-        print(icon)
-    return render_template('task.html', icons=icons)
+        if not title:
+            error_title = 'error_border'
+            return render_template('add_task.html', error_title=error_title)
+        db.execute("INSERT INTO tasks (userid, title, description, icon, status) VALUES (?, ?, ?, ?, ?)", user_id, title, description, icon, 'in_progress')
+        
+        
+        return redirect('/')
+    return render_template('add_task.html')
 
 # LOGIN PAGE
 @app.route('/login', methods=['GET', 'POST'])
@@ -111,7 +114,7 @@ def register():
         
         hash = generate_password_hash(password)
         
-        db.execute('INSERT INTO users (username, hash) VALUES (?, ?)', username, hash)
+        db.execute('INSERT INTO users (username, hash) VALUES (?, ?)', username.strip(), hash)
         flash('Account has been created')
         
         return redirect('/login')
@@ -119,8 +122,60 @@ def register():
     
     return render_template('register.html', error=error)
 
+
+# IN PROGRESS PAGE
+@app.route('/in_progress')
+@login_required
+def in_progress():
+    user_id = session['user_id']
+    in_progress_tasks = db.execute("SELECT id, icon, title FROM tasks WHERE userid = ? AND status = 'in_progress'", user_id)
+    return render_template('in_progress.html', in_progress_tasks=in_progress_tasks)
+
+@app.route('/in_progress/<task_id>', methods=["GET", "POST"])
+@login_required
+def edit_in_progress_task(task_id):
+    error_title = None
+    task_details = db.execute("SELECT id, title, description, icon, status FROM tasks WHERE id = ?", task_id)
+    if request.method == 'POST':
+        title = request.form.get('title')
+        if not title:
+            error_title = 'error_border'
+            return render_template('edit_task.html',task=task_details[0], error_title=error_title)
+        description = request.form.get('description')
+        icon = request.form.get('icon')
+        status = request.form.get('status')
+
+        db.execute("UPDATE tasks SET title = ?, description = ?, icon = ?, status = ? WHERE id = ?", 
+                   title, description, icon, status, task_id)
+        
+        if status == 'completed':
+            return redirect('/completed')
+        if status == 'wont_do':
+            return redirect('wont_do')
+        if status == "in_progress":
+            return redirect('/in_progress')
+        
+        
+    return render_template('edit_task.html', task=task_details[0])
+
+@app.route('/delete/<task_id>')
+@login_required
+def delete_task(task_id):
+    db.execute("DELETE FROM tasks WHERE id = ?", task_id)
+    return redirect('/in_progress')
+
 # COMPLETED PAGE
 @app.route('/completed')
 @login_required
 def completed():
-    return render_template('completed.html')
+    user_id = session['user_id']
+    completed_tasks = db.execute("SELECT id, icon, title FROM tasks WHERE userid = ? AND status = 'completed'", user_id)
+    return render_template('completed.html', completed_tasks=completed_tasks)
+
+# WONT DO PAGE
+@app.route('/wont_do')
+@login_required
+def wont_do():
+    user_id = session['user_id']
+    wont_do_tasks = db.execute("SELECT id, icon, title FROM tasks WHERE userid = ? AND status = 'wont_do'", user_id)
+    return render_template('wont_do.html', wont_do_tasks=wont_do_tasks)
